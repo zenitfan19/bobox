94b77452675356b93446838d8dd21557fcebf25b
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-01-15 18:59:18 +0300

    main-page: added gallery

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+	let firstSlider = $('.first-slider');
+
+	firstSlider.slick({
+		arrows: false,
+		dots: true,
+		autoplay: true
+	});
+
+	$(document).on('click', '.first-slider__arrow-prev', function(){
+        firstSlider.slick('slickPrev');
+        return false;
+    });
+
+    $(document).on('click', '.first-slider__arrow-next', function(){
+        firstSlider.slick('slickNext');
+        return false;
+    });
+
 	$(document).on('click', '.header-nav-list-item:not(.active)', function() {
 		$(this).siblings('.header-nav-list-item').removeClass('active');
 		$(this).addClass('active');
@@ -8,36 +26,31 @@ $(document).ready(function() {
 	});
 
 	$(document).on('click', '.header-nav-search-bar', function(e){
-
-		if (!$(e.target).closest('.header-nav-search-bar_wrapper').length) {
-			$('.header-nav-search-bar').closest('.header-nav-search-bar_wrapper').removeClass('active');
-			$('.header-nav-search-bar').find('.header-nav-search-bar__search-input').removeClass('active');
-			$('.header-nav-search-bar').removeClass('active');
-			$('.header-nav-list').removeClass('active');
-			$('.header-nav-list-item').removeClass('active');
-			$('.header-nav-list-item-ul').removeClass('active');
-			return;
-		} else {
+		if ($(e.target).closest('.header-nav-search-bar_wrapper').length) {
 			$(this).closest('.header-nav-search-bar_wrapper').addClass('active');
 			$(this).find('.header-nav-search-bar__search-input').addClass('active');
 			$(this).addClass('active');
 			$('.header-nav-list').addClass('active');
 			$('.header-nav-list-item').removeClass('active');
 			$('.header-nav-list-item-ul').removeClass('active');
-		}
+		};
 	});
 
-	// $(document).on('click', function(e){
+	$(document).on('click', function(e){
+		if (!$(e.target).closest('.header-nav-search-bar_wrapper').length) {
+			$('.header-nav-search-bar').closest('.header-nav-search-bar_wrapper').removeClass('active');
+			$('.header-nav-search-bar').find('.header-nav-search-bar__search-input').removeClass('active');
+			$('.header-nav-search-bar').removeClass('active');
+			$('.header-nav-list').removeClass('active');
+		};
+	});
 
-	// 	console.log($(e.target).closest('.header-nav-search-bar_wrapper').length);
-
-	// 	if (!$(e.target).closest('.header-nav-search-bar_wrapper').length) {
-	// 		$('.header-nav-search-bar').closest('.header-nav-search-bar_wrapper').removeClass('active');
-	// 		$('.header-nav-search-bar').find('.header-nav-search-bar__search-input').removeClass('active');
-	// 		$('.header-nav-search-bar').removeClass('active');
-	// 		$('.header-nav-list').removeClass('active');
-	// 		$('.header-nav-list-item').removeClass('active');
-	// 		$('.header-nav-list-item-ul').removeClass('active');
-	// 	}
-	// })
+	$(document).on('click', function(e){
+		console.log($(e.target).closest('.header-nav-list-item-ul.active').length );
+		console.log($(e.target).closest('.header-nav-list-item').length);
+		if(!$(e.target).closest('.header-nav-list-item-ul.active').length && !$(e.target).closest('.header-nav-list-item').length) {
+			$('.header-nav-list-item-ul.active').removeClass('active');
+			$('.header-nav-list-item').removeClass('active');
+		}
+	});
 });
