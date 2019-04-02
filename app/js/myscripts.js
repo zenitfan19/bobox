2e21ed951bd66bfe8e9e2f7cb529b10faa6d9c8e
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-04-02 14:40:30 +0300

    finished mockup

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -680,6 +680,12 @@ $(document).ready(function() {
 						slidesToShow: 1,
 						slidesToScroll: 1,
 						dots: true
+					}, 
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						dots: false
 					}
 				}
 			]
@@ -874,10 +880,17 @@ $(document).ready(function() {
 			slidesToScroll: 1,
 			responsive: [
 				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						draggable: true	
+					}				
+				},
+				{
 					breakpoint: 480,
 					slidesToShow: 1,
 					draggable: true
-				}
+				}				
 			]
 		});
 		a.on('click', function(){
